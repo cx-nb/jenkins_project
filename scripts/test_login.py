53f8f666ca94e740b4bda77bfec348386c97723e
陈导 <cx13062555152@163.com>
2020-06-28 17:19:53 +0800

    first commit

--- scripts/test_login.py
+++ scripts/test_login.py
@@ -5,11 +5,12 @@ class TestLogin:
 
     @allure.step(title="小吴是猪")
     def test_login_001(self):
-        assert 0
+        assert 1
 
     @allure.step(title="小吴是dog")
     def test_login_002(self):
         assert 1
 
+    @allure.step(title="小吴是feipig")
     def test_login_003(self):
         assert 1
